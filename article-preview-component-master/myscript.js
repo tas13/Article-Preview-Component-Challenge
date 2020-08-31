7432c0b5d398b359273f45ded6bca8f70e51a0c6
tas13 <dreamlesssnow@aol.com>
2020-08-31 11:18:03 +0200

    Latest commit

--- article-preview-component-master/myscript.js
+++ article-preview-component-master/myscript.js
@@ -1,7 +1,8 @@
 document.getElementById("share").addEventListener("click", remover)
 
 function remover() {
- console.log(document.getElementById("shareLinks").style.display = "")
+
  document.getElementById("share").src = "images/icon-share2.svg"
  document.getElementById("share").style.backgroundColor= "hsl(214, 17%, 51%)"
+ document.getElementById("shareLinks").style.opacity = '1'
 }
